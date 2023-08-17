8ce307d574e1873aa317992ecc222b1c3b1430d3
Victor Hugo Viana <victor.santos@acocearense.com.br>
2023-08-17 11:34:31 -0300

    Upgrade project to .NET Standard 2.0

--- Chaso.Push.Client.Test/Properties/AssemblyInfo.cs
+++ Chaso.Push.Client.Test/Properties/AssemblyInfo.cs
@@ -1,20 +1,9 @@
 using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
-
-[assembly: AssemblyTitle("Chaso.Push.Client.Test")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Chaso.Push.Client.Test")]
-[assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
 [assembly: ComVisible(false)]
 
 [assembly: Guid("02fd8dde-4754-45f4-982a-ef7abbb75e80")]
-
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
